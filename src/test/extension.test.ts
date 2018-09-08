73a2fb510303a3942abdd0ef350a10b5abba336d
shahid <shahid@linasmatkasse.se>
2018-09-08 19:19:41 +0200

    initial commit

---
+++ src/test/extension.test.ts
@@ -0,0 +1,30 @@
+//
+// Note: This example test is leveraging the Mocha test framework.
+// Please refer to their documentation on https://mochajs.org/ for help.
+//
+
+// The module 'assert' provides assertion methods from node
+import * as assert from 'assert';
+// import * as vscode from 'vscode';
+// import * as ToggleFormatOnSave from '../extension';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+// import * as vscode from 'vscode';
+// import * as myExtension from '../extension';
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Toggle FormatOnSave Tests", function () {
+
+    // Defines a Mocha unit test
+    test("FormatOnSave activates", function () {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+        // const config = v scode.workspace.getConfiguration('editor');
+        // assert.equal(false, config.get('formatOnSave'));
+
+        // ToggleFormatOnSave.activate({});
+        
+        
+    });
+});
